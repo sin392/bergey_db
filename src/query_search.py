7c7b2bc9113ddb4628ce031612369872a86ba6d4
Shin <sin392@gmail.com>
2019-12-05 22:52:20 +0900

    small change

--- src/query_search.py
+++ src/query_search.py
@@ -2,6 +2,7 @@
 import json
 from time import time
 import re
+from io import StringIO
 
 # %%
 input_path = "./json/BERGEY’S_MANUAL_sample.json"
@@ -11,21 +12,32 @@ with open(input_path, "r") as f:
 
 # %%
 start = time()
+log = StringIO(initial_value=f"query : {keyward_list}\n")
+
 for i, page in str_dict.items():
     found_words = []
     for keyward in keyward_list:
         # N = len(re.findall(keyward.lower(), outfp.getvalue().lower()))
-        N = page.lower().count(keyward.lower())
-        # N = 0
-        # for x in [x.lower() for x in page.split("\n") if x != " " and len(keyward) <= len(x)]:
-        #     N += x.count(keyward.lower())
+        # N = page.lower().count(keyward.lower())
+        query = keyward.lower()
+        match_idx_list = [m.start() for m in re.finditer(query, page.lower())]
+        N = len(match_idx_list)
+
         if N > 0:
             found_words.append(f"「{keyward}」: {N} found")
+            for j in range(N):
+                idx = match_idx_list[j]
+                string = page[idx-30:idx+30].replace("\n", " ").strip()
+                found_words.append(f"{j} : {string}")
     if len(found_words) > 0:
-        print(i)
-        [print(x) for x in found_words]
-        print("#"*40)
+        print(str(i), file=log)
+        [print(x, file=log) for x in found_words]
+        print("#"*80, file=log)
 end = time()
+print(log.getvalue())
+
+with open("log.txt", "w") as f:
+    f.write()
 
 print("SIZE :", len(str_dict))
 print("TIME :", end - start)
