74f34c11db38b1bf88a35c64197a9a3fc149e8c3
Shin <sin392@gmail.com>
2019-12-15 22:12:59 +0900

    add html log

--- src/query_search.py
+++ src/query_search.py
@@ -2,7 +2,10 @@
 import json
 from time import time
 import re
+import sys
 from io import StringIO
+sys.path.append("src")
+from utils import make_html
 
 # %%
 input_path = "./json/BERGEY’S_MANUAL-Betaproteobacteria.json"
@@ -12,7 +15,9 @@ with open(input_path, "r") as f:
 # %%
 with open("verb-list.txt", "r") as f:
     verb_list = f.read().split("\n")
-print(verb_list)
+
+with open("log.html", "w") as f:
+    f.write("")
 # %%
 start = time()
 log = StringIO()
@@ -32,6 +37,7 @@ for i, page in str_dict.items():
                 idx = match_idx_list[j]
 
                 try:
+                    # 何も見つからないとNoneになりstart()でエラー
                     h_idx = idx - (1 + re.search(r"[A-Z] \.", page[:idx:-1]).start())
                 except:
                     # h_idx = idx - (1 + page[:idx].rfind("\n"))
@@ -44,7 +50,7 @@ for i, page in str_dict.items():
                 string = page[h_idx:t_idx].replace("\n", " ").strip()
 
                 used_verbs = [x for x in verb_list if x in string]
-
+                make_html(string, keyward, idx, used_verbs, None)
                 found_words.append(f"{j} : {string}")
                 found_words.append(f"used_verbs : {used_verbs}")
     if len(found_words) > 0:
