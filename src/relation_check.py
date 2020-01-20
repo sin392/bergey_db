6897baf5fedccedd26b0d57e827a122b1facf036
Shin <sin392@gmail.com>
2020-01-20 21:30:49 +0900

    update

--- src/relation_check.py
+++ src/relation_check.py
@@ -13,7 +13,7 @@ with open("../json/splited_sentence.json") as f:
 with open("../txt/search-dictionary_single.txt", "r") as f:
     # keywords = list(map(invalid_re, [x.strip() for x in f.readlines() if len(x.strip()) > 2]))
     keywords = [x.strip() for x in f.readlines() if len(x.strip()) > 2]
-    keywords = set(keywords)
+    # keywords = set(keywords)
 with open("../csv/verb-list.csv", "r") as f:
     verbs_labels = [x.strip().split(",") for x in f.readlines()]
 
@@ -44,15 +44,21 @@ for microbe, sentence in tqdm(sentences_dict.items()):
                 string = sentence[h_idx:t_idx]
                 # print(string)
                 for verb, label in verbs_labels:
+                    splited_string = string.split()
                     if verb in string:
                         labels.append(label)
+                if labels == []:
+                    if "no" in splited_string or "not" in splited_string or "unable" in splited_string:
+                        labels.append(0)
+                    else:
+                        labels.append(1)
 
         label = np.argmax(np.bincount(labels)) if len(labels) > 0 else -1
         if label != -1:
             symbol = "-" if label == 0 else "+"
             df.at[raw_keyword, microbe] = symbol
-            print(df.at[raw_keyword, microbe], label, labels)
-            print(microbe, raw_keyword, label)
+            # print(df.at[raw_keyword, microbe], label, labels)
+            # print(microbe, raw_keyword, label)
         # break
 
 df.to_csv("../csv/output.csv")
